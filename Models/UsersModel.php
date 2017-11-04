f53222f369a4a08b98af666997728fd864025c2b
Jeff Mueller <muelljef@oregonstate.edu>
2017-11-04 13:55:05 +1000

    refactoring queries

--- Models/UsersModel.php
+++ Models/UsersModel.php
@@ -14,35 +14,28 @@ require_once __DIR__ . '/BaseModel.php';
 class UsersModel extends BaseModel
 {
     /**
+     * Get all the users
+     *
      * @return array
      * @throws Exception
      */
     public static function getUsers()
     {
-        $mysqli = self::getConnection();
-
         $query = "SELECT e.ID, CONCAT_WS(' ', e.fName, e.lName) fullName, e.hireDate, t.Type
             FROM Employees e
             JOIN UserType t ON e.ID = t.EmployeeID";
 
-        $employeeStmt = $mysqli->prepare($query);
-        if (!$employeeStmt) {
-            $errorMsg = "Prepare failed: ({$mysqli->errno}) {$mysqli->error}";
-            throw new Exception($errorMsg);
-        }
-
-        if(!$employeeStmt->execute()) {
-            $errorMsg = "Execute failed: {$employeeStmt->errno} {$employeeStmt->error}";
-            throw new Exception($errorMsg);
-        }
-
-        return $employeeStmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        return self::runQuery($query);
     }
 
+    /**
+     * Get a user by id
+     *
+     * @param int $userId
+     * @return array
+     */
     public static function getUser($userId)
     {
-        $mysqli = self::getConnection();
-
         $query = "
             SELECT e.ID, e.fName, e.lName, e.hireDate, e.Email, t.Type
             FROM Employees e
@@ -50,25 +43,16 @@ class UsersModel extends BaseModel
             WHERE e.ID = ?
         ";
 
-        $employeeStmt = $mysqli->prepare($query);
-        if (!$employeeStmt) {
-            $errorMsg = "Prepare failed: ({$mysqli->errno}) {$mysqli->error}";
-            throw new Exception($errorMsg);
-        }
-
-        if (!$employeeStmt->bind_param('i', $userId)) {
-            $errorMsg = "Bind failed: {$employeeStmt->errno} {$employeeStmt->error}";
-            throw new Exception($errorMsg);
-        }
-
-        if(!$employeeStmt->execute()) {
-            $errorMsg = "Execute failed: {$employeeStmt->errno} {$employeeStmt->error}";
-            throw new Exception($errorMsg);
-        }
-
-        return $employeeStmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        return self::runQuery($query, ['i', $userId]);
     }
 
+    /**
+     * Add a user
+     *
+     * @param array $data
+     * @return bool
+     * @throws Exception
+     */
     public static function addUser($data)
     {
         $firstName = $data['fName'];
@@ -78,51 +62,23 @@ class UsersModel extends BaseModel
         $password = $data['Password'];
         $type = $data['Type'];
 
-        $mysqli = self::getConnection();
-
         $query = "
             INSERT INTO Employees (fName, lName, hireDate, Email, Password, CreatedOn)
             VALUES (?, ?, ?, ?, ?, CURRENT_TIMESTAMP)
         ";
 
-        $employeeStmt = $mysqli->prepare($query);
-        if (!$employeeStmt) {
-            $errorMsg = "Prepare failed: ({$mysqli->errno}) {$mysqli->error}";
-            throw new Exception($errorMsg);
-        }
-
-        if (!$employeeStmt->bind_param('sssss', $firstName, $lastName, $hireDate, $email, $password)) {
-            $errorMsg = "Bind failed: {$employeeStmt->errno} {$employeeStmt->error}";
-            throw new Exception($errorMsg);
-        }
-
-        if(!$employeeStmt->execute()) {
-            $errorMsg = "Execute failed: {$employeeStmt->errno} {$employeeStmt->error}";
-            throw new Exception($errorMsg);
-        }
-        // Get the employee ID for the UserType addition
-        $employeeId = $mysqli->insert_id;
+        $employeeId = self::runQuery(
+            $query,
+            ['sssss', $firstName, $lastName, $hireDate, $email, $password],
+            'insert_get_id'
+        );
 
         $query = "
             INSERT INTO UserType (EmployeeID, Type)
             VALUES (?, ?)
         ";
 
-        $userTypeStmt = $mysqli->prepare($query);
-        if (!$userTypeStmt) {
-            $errorMsg = "Prepare failed: ({$mysqli->errno}) {$mysqli->error}";
-            throw new Exception($errorMsg);
-        }
-
-        if (!$userTypeStmt->bind_param('is', $employeeId, $type)) {
-            $errorMsg = "Bind failed: {$userTypeStmt->errno} {$userTypeStmt->error}";
-            throw new Exception($errorMsg);
-        }
-
-        if(!$userTypeStmt->execute()) {
-            $errorMsg = "Execute failed: {$userTypeStmt->errno} {$userTypeStmt->error}";
-            throw new Exception($errorMsg);
-        }
+        self::runQuery($query, ['is', $employeeId, $type], 'insert_get_id');
 
         return true;
     }
